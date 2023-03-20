6a9b9ac1b0f4935dc75bf0f42c9a2a09eb4a5a73
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2023-03-20 23:53:39 +0100

    Main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InputAreaComponent } from './input-area/input-area.component';
+import { RecaptchaModule } from 'ng-recaptcha';
+import { FormsModule } from '@angular/forms';
+import { ScreenshotToolComponent } from './input-area/screenshot-tool/screenshot-tool.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, InputAreaComponent, ScreenshotToolComponent],
+  imports: [BrowserModule, AppRoutingModule, RecaptchaModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
